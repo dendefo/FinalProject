c6446b163ccaab655fe9f0ba9a6f5abcc77cb63c
dendefo <daniek332@gmail.com>
2024-05-08 03:28:09 +0300

    Revert "renderer changes"
    This reverts commit 4ea7ce9679d9ab1332f26b6ecdf04b7edeba2b95.

--- Renderer/ConsoleRenderer.cs
+++ Renderer/ConsoleRenderer.cs
@@ -16,13 +16,13 @@ namespace Renderer
             Console.SetCursorPosition(1, 0);
             for (int i = 0; i < scene.Width; i++)
             {
-                Console.SetCursorPosition((i + 1) * 3, 0);
+                Console.SetCursorPosition((i + 1) * 3 - 1, 0);
                 Console.Write(IRenderer.ConvertIntToString(i + 1));
             }
             Console.SetCursorPosition(1, scene.Height + 1);
             for (int i = 0; i < scene.Width; i++)
             {
-                Console.SetCursorPosition((i + 1) * 3, scene.Height + 1);
+                Console.SetCursorPosition((i + 1) * 3 - 1, scene.Height + 1);
                 Console.Write(IRenderer.ConvertIntToString(i + 1));
             }
 
